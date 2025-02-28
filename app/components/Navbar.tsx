af695710b1551d8b01023b0be52d810cfe3a8e3d
Alvaro Canicio <aritomix@yahoo.es>
2025-02-28 11:04:05 +0100

    Weapon page development 1

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -30,10 +30,10 @@ export default function Navbar() {
                   {/* Dropdown oculto por defecto */}
                   <div className="absolute hidden group-hover:block w-48 rounded-md shadow-lg bg-white z-10">
                     <div className="py-1" role="menu" aria-orientation="vertical" aria-labelledby="options-menu-button">
-                    <a href="#" onClick={() => router.push('/dashboard')} className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" role="menuitem">Resumen de partidas</a>
+                      <a href="#" onClick={() => router.push('/dashboard')} className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" role="menuitem">Resumen de partidas</a>
                       <a href="#" onClick={() => router.push('/totaldata')} className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" role="menuitem">Estadísticas</a>
                       <a href="#" onClick={() => router.push('/matchList')} className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" role="menuitem">Lista de partidas</a>
-                      <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" role="menuitem">Lista de armas</a>
+                      <a href="#" onClick={() => router.push('/weapons')} className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" role="menuitem">Lista de armas</a>
                       <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" role="menuitem">Títulos</a>
                       {/* Añade aquí las subopciones de Estadísticas */}
                     </div>
---
+++ app/weapons/page.tsx
@@ -0,0 +1,256 @@
+"use client";
+
+import React, { useState, useEffect, useCallback } from 'react';
+import Navbar from '../components/Navbar';
+import { useRouter } from 'next/navigation';
+import DateRangePicker from '../components/DateRangePicker';
+import moment from 'moment';
+import {
+  Table,
+  TableHeader,
+  TableColumn,
+  TableBody,
+  TableRow,
+  TableCell,
+  getKeyValue,
+  SortDescriptor
+} from "@heroui/react";
+import Image from 'next/image';
+import LoadingSpinner from '../components/Spinner';
+import { RangeValue } from "@heroui/react";
+import { DateValue, parseDate } from "@internationalized/date";
+
+// TODO: - Adaptar tamaños de columnas. Las de jugadores deben ser iguales, las de iconos y nombres deben ser fijas y más pequeñas
+// TODO: - Añadir sorted en las armas. Todas las columnas excepto icon
+// TODO: - Añadir Contadores de armas
+// TODO: - Cambiar funcionamiento date picker para que busque al pulsar siempre y busque al seleccionar del desplegable.
+// TODO: - Pantalla títulos
+
+
+export default function WeaponsPage() {
+  const [weaponsData, setWeaponsData] = useState<any | null>(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
+  const [startDate, setStartDate] = useState<moment.Moment | null>(null);
+  const [endDate, setEndDate] = useState<moment.Moment | null>(null);
+  const router = useRouter();
+
+  const fetchData = async (startDate: string, endDate: string) => {
+    setIsLoading(true);
+    setError(null);
+
+    const token = localStorage.getItem('jwt_token');
+
+    if (!token) {
+      setError('No se encontró token de autenticación.');
+      setIsLoading(false);
+      router.push('/login');
+      return;
+    }
+
+    const apiUrl = `http://localhost:8080/api/weapons/getWeaponData?startDate=${startDate}&endDate=${endDate}`;
+
+    try {
+      const response = await fetch(apiUrl, {
+        headers: {
+          'Authorization': `Bearer ${token}`,
+          'Content-Type': 'application/json',
+        },
+      });
+
+      if (!response.ok) {
+        let errorMessage = `Error al obtener datos de armas. Status: ${response.status}`;
+        try {
+          const errorData = await response.json();
+          if (errorData && errorData.message) {
+            errorMessage += ` - ${errorData.message}`;
+          }
+        } catch (jsonError) {
+          console.error('Error parsing error JSON response:', jsonError);
+        }
+        throw new Error(errorMessage);
+      }
+
+      const data = await response.json();
+      setWeaponsData(data);
+      setIsLoading(false);
+
+    } catch (apiError: any) {
+      console.error('Error al hacer la petición a la API:', apiError);
+      setError('Error al cargar datos de armas. Por favor, intenta de nuevo más tarde.');
+      setIsLoading(false);
+    }
+  };
+
+  const [value, setValue] = useState<RangeValue<DateValue>>({
+    start: startDate ? parseDate(startDate.format('YYYY-MM-DD')) : parseDate("2023-09-01"),
+    end: endDate ? parseDate(endDate.format('YYYY-MM-DD')) : parseDate("2099-01-01")
+  });
+  
+  useEffect(() => {
+    const storedValue = localStorage.getItem('dateRange');
+      
+    if (typeof window !== "undefined") {
+      const token = localStorage.getItem("jwt_token");
+    
+      if (!token) {
+        router.push('/login');
+      } else {
+        validateToken(token).then((validationResult) => {
+          if (!validationResult.isValid) {
+            router.push('/login');
+          } else {
+            if (storedValue) {
+              console.log(storedValue);
+              const parsedValue = JSON.parse(storedValue);
+              setValue({
+                start: parseDate(parsedValue.start),
+                end: parseDate(parsedValue.end)
+              });
+              // Usar directamente los valores parseados
+              fetchData(parsedValue.start, parsedValue.end);
+            } else {
+              setValue({
+                start: startDate ? parseDate(startDate.format('YYYY-MM-DD')) : parseDate("2023-09-01"),
+                end: endDate ? parseDate(endDate.format('YYYY-MM-DD')) : parseDate("2099-01-01")
+              });
+              // Usar los valores por defecto
+              fetchData(
+                startDate ? startDate.format('YYYY-MM-DD') : "2023-09-01", 
+                endDate ? endDate.format('YYYY-MM-DD') : "2099-01-01"
+              );
+            }
+          }
+        });
+      }
+    }
+  }, [startDate, endDate]);
+
+  const handleDateRangeChange = (startDate: moment.Moment | null, endDate: moment.Moment | null) => {
+    setStartDate(startDate);
+    setEndDate(endDate);
+    if (startDate && endDate) {
+      fetchData(startDate.format('YYYY-MM-DD'), endDate.format('YYYY-MM-DD'));
+    }
+  };
+
+  if (isLoading) {
+    return (
+      <div>
+      <Navbar />
+      <main className="bg-gray-100 py-6 flex justify-center items-center h-screen">
+        <div className="flex flex-col items-center">
+        <LoadingSpinner />
+        <p className="mt-4">Cargando contadores...</p>
+        </div>
+      </main>
+      </div>
+    );
+  }
+
+  if (error) {
+    return (
+      <div>
+        <Navbar />
+        <main className="bg-gray-100 py-6 flex justify-center items-center h-screen">
+          <p className="text-red-500">Error al cargar contadores: {error}</p>
+        </main>
+      </div>
+    );
+  }
+
+  if (!weaponsData) {
+    return (
+      <div>
+        <Navbar />
+        <main className="bg-gray-100 py-6 flex justify-center items-center h-screen">
+          <p>No hay datos disponibles de armas.</p>
+        </main>
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      <Navbar />
+      <main className="bg-gray-100 py-6">
+        <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+          <div className="flex justify-between items-center mb-4">
+            <h1 className="text-2xl font-semibold text-gray-900">Armas</h1>
+            <DateRangePicker
+              onDateRangeChange={handleDateRangeChange}
+              startDate={startDate}
+              endDate={endDate}
+            />
+          </div>
+
+          <Table
+            isCompact
+            isStriped
+            aria-label="Datos totales"
+          >
+            <TableHeader>
+              <TableColumn key="icon">
+                &nbsp;
+              </TableColumn>
+              <TableColumn key="data">
+                &nbsp;
+              </TableColumn>
+              <TableColumn key="shinchan" style={{ color: '#262626', background: 'linear-gradient(to right, rgba(104,163,229,0) 0%, rgba(104,163,229,0.75) 50% 90%, rgba(104,163,229,0) 100%)', borderRadius: '16px', textAlign: 'right' }}>
+                ShinChan&nbsp;&nbsp;
+              </TableColumn>
+              <TableColumn key="kazama" style={{ color: '#262626', background: 'linear-gradient(to right, rgba(237,163,56,0) 0%, rgba(237,163,56,0.75) 50% 90%, rgba(237,163,56,0) 100%)', borderRadius: '16px', textAlign: 'right' }}>
+                Kazama&nbsp;&nbsp;
+              </TableColumn>
+              <TableColumn key="nene" style={{ color: '#262626', background: 'linear-gradient(to right, rgba(230,241,61,0) 0%, rgba(230,241,61,0.75) 50% 90%, rgba(230,241,61,0) 100%)', borderRadius: '16px', textAlign: 'right' }}>
+                Nene&nbsp;&nbsp;
+              </TableColumn>
+              <TableColumn key="swagchan" style={{ color: '#262626', background: 'linear-gradient(to right, rgba(128,60,161,0) 0%, rgba(128,60,161,0.75) 50% 90%, rgba(128,60,161,0) 100%)', borderRadius: '16px', textAlign: 'right' }}>
+                SwagChan&nbsp;&nbsp;
+              </TableColumn>
+              <TableColumn key="mafios" style={{ color: '#262626', background: 'linear-gradient(to right, rgba(16,152,86,0) 0%, rgba(16,152,86,0.75) 50% 90%, rgba(16,152,86,0) 100%)', borderRadius: '16px', textAlign: 'right' }}>
+                The Mafios&nbsp;&nbsp;
+              </TableColumn>
+            </TableHeader>
+            <TableBody items={weaponsData}>
+              {(item: any) => (
+                <TableRow key={getKeyValue(item, "weapon")}>
+                  <TableCell style={{ width: '150px' }}><Image src={item.icon || '/default-icon.png'} alt="icon" width={150} height={30} /></TableCell>
+                  <TableCell>{item.weapon}</TableCell>
+                  <TableCell style={{ background: 'linear-gradient(to right, rgba(104,163,229,0) 0%, rgba(104,163,229,0.75) 50% 90%, rgba(104,163,229,0) 100%)', textAlign: 'right' }}>{item.shinchan}&nbsp;&nbsp;</TableCell>
+                  <TableCell style={{ background: 'linear-gradient(to right, rgba(237,163,56,0) 0%, rgba(237,163,56,0.75) 50% 90%, rgba(237,163,56,0) 100%)', textAlign: 'right' }}>{item.kazama}&nbsp;&nbsp;</TableCell>
+                  <TableCell style={{ background: 'linear-gradient(to right, rgba(230,241,61,0) 0%, rgba(230,241,61,0.75) 50% 90%, rgba(230,241,61,0) 100%)', textAlign: 'right' }}>{item.nene}&nbsp;&nbsp;</TableCell>
+                  <TableCell style={{ background: 'linear-gradient(to right, rgba(128,60,161,0) 0%, rgba(128,60,161,0.75) 50% 90%, rgba(128,60,161,0) 100%)', textAlign: 'right' }}>{item.swagchan}&nbsp;&nbsp;</TableCell>
+                  <TableCell style={{ background: 'linear-gradient(to right, rgba(16,152,86,0) 0%, rgba(16,152,86,0.75) 50% 90%, rgba(16,152,86,0) 100%)', textAlign: 'right' }}>{item.mafios}&nbsp;&nbsp;</TableCell>
+                </TableRow>
+              )}
+            </TableBody>
+          </Table>
+
+        </div>
+      </main>
+    </div>
+  );
+}
+
+async function validateToken(token: string): Promise<{ isValid: boolean }> {
+  const backendValidationEndpoint = 'http://localhost:8080/api/validation/token';
+  try {
+    const response = await fetch(backendValidationEndpoint, {
+      method: 'POST',
+      headers: {
+        'Authorization': `Bearer ${token}`,
+        'Content-Type': 'application/json',
+      },
+    });
+
+    if (response.ok) {
+      return { isValid: true };
+    } else {
+      return { isValid: false };
+    }
+  } catch (error) {
+    console.error('Error al validar el token con el backend:', error);
+    return { isValid: false };
+  }
+}
