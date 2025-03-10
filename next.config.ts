1d3fb4bb7e67ec35e4304ed1622da229925bd182
Alvaro Canicio <aritomix@yahoo.es>
2025-03-10 21:35:12 +0100

    ignore eslint

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,13 @@
 // next.config.ts
+
 const nextConfig = {
   images: {
     domains: ['i.ytimg.com'],
   },
-  // Your other existing config options...
+  eslint: {
+    ignoreDuringBuilds: true, // Ignora los errores de ESLint durante la compilación
+  },
+  // Tus otras opciones de configuración existentes...
 }
 
 export default nextConfig;
