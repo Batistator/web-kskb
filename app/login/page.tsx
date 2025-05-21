63012e266780de2b012e652ec85a39d65683b68d
Batistator <83245185+Batistator@users.noreply.github.com>
2025-05-21 10:26:34 +0200

    Update page.tsx

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -174,7 +174,7 @@ export default function LoginPage() {
             <form className="space-y-6" onSubmit={handleSubmit}>
               <div>
                 <label htmlFor="username" className="block text-sm font-medium text-gray-700">
-                  Nombre de usuario
+                  Nombre de bito
                 </label>
                 <div className="mt-1">
                   <input
